bc5f76055b2aeea972b62124ddf57a662d2588b0
KlenkF <KlenkF@IR1-PC-11-W7.schule.local>
2015-10-21 16:51:16 +0200

    change code look f ( x ) { --> f(x){

--- für die Schule/src/für/die/schule/FürDieSchule.java
+++ für die Schule/src/für/die/schule/FürDieSchule.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
+/* To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package für.die.schule;
 
 /**
- *
  * @author KlenkF
  */
 public class FürDieSchule {
-
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        
     }
     
 }
